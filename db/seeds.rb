02fed1524c333749078115906585c221c618b2c2
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-01-02 22:12:09 +0900

    管理者用コントローラーの実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
-User.create!(username:  "管理者",
+User.create!(
+  name:  "管理者",
   email: "admin@example.jp",
+  f_name: "あどみん",
+  l_name: "あどみん",
+  f_name_r: "アドミン",
+  l_name_r: "アドミン",
   password:  "admin11",
   password_confirmation: "admin11",
-  admin: true)
+  birthday: "1994-11-24",
+  p_num: "08012345678",
+  admin: true
+)
