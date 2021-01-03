2e709e18f8bf13e1c0e55f8ac6700b478e9e3a2a
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-01-03 12:54:16 +0900

    単体テストコードの実装

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :shop do
-    
+    image           {Faker::Lorem.sentence}
+    shop_name       {"テスト"}
+    profile         {"テスト"}
+    genre_id        {2}
+    p_num           {"0561-12-3456"}
+    p_code          {"123-4567"}
+    prefecture_id   {2}
+    city            {"テスト"}
+    address         {"テスト"}
+    building        {"テスト"}
+
+    association :user
   end
 end
