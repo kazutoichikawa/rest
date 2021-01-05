021bb715bec88515d664ff18eb723e3bccef6d04
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-01-05 18:49:51 +0900

    マイグレートションファイルの編集

--- db/migrate/20210105075725_create_reservations.rb
+++ db/migrate/20210105075725_create_reservations.rb
@@ -1,7 +1,11 @@
 class CreateReservations < ActiveRecord::Migration[6.1]
   def change
     create_table :reservations do |t|
-
+      t.date :date ,        null: false
+      t.time :time ,        null: false
+      t.integer :n_people,  null: false
+      t.references :user,   foreign_key: true
+      t.references :shop,   foreign_key: true
       t.timestamps
     end
   end
