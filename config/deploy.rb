a087a8fd42b18efd5b1133da462a30742a4a82f7
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-02-24 16:37:06 +0900

    AWSEC2の設定

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+lock "~> 3.15.0"
+
+set :application, 'rest'
+set :repo_url,  'git@github.com:kazutoichikawa/https://github.com/kazutoichikawa/rest.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/kazuto_key_pair.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
