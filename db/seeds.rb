49d779383df5335ff00bded12ac33d5178719803
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-03-04 12:33:36 +0900

    コミット

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,45 @@
-# User.create!(
-#   name:  "管理者",
-#   email: "admin@example.jp",
-#   f_name: "あどみん",
-#   l_name: "あどみん",
-#   f_name_r: "アドミン",
-#   l_name_r: "アドミン",
-#   password:  "admin11",
-#   password_confirmation: "admin11",
-#   birthday: "1994-11-24",
-#   p_num: "08012345678",
-#   admin: true
-# )
-# User.create!(
-#   name:  "サンプル",
-#   email: "sample@sample.jp",
-#   f_name: "サン",
-#   l_name: "プル",
-#   f_name_r: "サン",
-#   l_name_r: "プル",
-#   password:  "sample11",
-#   password_confirmation: "sample11",
-#   birthday: "1994-11-24",
-#   p_num: "08012345678",
-#   admin: false
-# )
+User.create!(
+  name:  "管理者",
+  email: "admin@example.jp",
+  f_name: "あどみん",
+  l_name: "あどみん",
+  f_name_r: "アドミン",
+  l_name_r: "アドミン",
+  password:  "admin11",
+  password_confirmation: "admin11",
+  birthday: "1994-11-24",
+  p_num: "08012345678",
+  admin: true
+)
+User.create!(
+  name:  "サンプル",
+  email: "sample@sample.jp",
+  f_name: "サン",
+  l_name: "プル",
+  f_name_r: "サン",
+  l_name_r: "プル",
+  password:  "sample11",
+  password_confirmation: "sample11",
+  birthday: "1994-11-24",
+  p_num: "08012345678",
+  admin: false
+)
 
-30.times do |m|
-  Shop.create(
-    shop_name: "店#{m + 1}",
-    genre_id: 1,
-    tag_id: 1,
-    profile: "店#{m + 1}の詳細が表示されます",
-    p_num: "0561-12-3456",
-    p_code: "123-4567",
-    prefecture_id: m + 1,
-    city: "テスト市",
-    address: "12-3",
-    building: "テストビル",
-    user_id: 1,
-  )
-end
+# 30.times do |m|
+#   Shop.create(
+#     shop_name: "店#{m + 1}",
+#     genre_id: 1,
+#     tag_id: 1,
+#     profile: "店#{m + 1}の詳細が表示されます",
+#     p_num: "0561-12-3456",
+#     p_code: "123-4567",
+#     prefecture_id: m + 1,
+#     city: "テスト市",
+#     address: "12-3",
+#     building: "テストビル",
+#     user_id: 1,
+#   )
+# end
 
 
 
